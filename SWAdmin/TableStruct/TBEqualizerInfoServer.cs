d25060e713e87078944f7f11283658aedab899b7
GreenDamTan <GreenDam@makedie.xyz>
2021-01-21 22:36:06 +0800

    ADD:TBEqualizerInfoServer

---
+++ SWAdmin/TableStruct/TBEqualizerInfoServer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAdmin.TableStruct
+{
+    public class TBEqualizerInfoServer : BaseStruct
+    {
+        public TBEqualizerInfo[] lsData;
+        public SWString md5_ = new SWString();
+
+        public TBEqualizerInfoServer() { }
+
+        public override void beforeRead()
+        {
+            
+        }
+
+        public override void beforeWrite()
+        {
+            
+        }
+
+        public override void read(SWReader reader)
+        {
+            
+        }
+
+        public override void write(SWWriter writer)
+        {
+            
+        }
+
+        public class TBEqualizerInfo : BaseStruct
+        {
+            public UInt32 Equalizer_ID;
+          
+            public override void beforeRead()
+            {
+                
+            }
+
+            public override void beforeWrite()
+            {
+                
+            }
+
+            public override void read(SWReader reader)
+            {
+                
+            }
+
+            public override void write(SWWriter writer)
+            {
+                
+            }
+        }
+    }
+}
