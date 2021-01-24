899e92873d14873a9aa41de699c127705579f50d
GreenDamTan <GreenDam@makedie.xyz>
2021-01-24 14:44:17 +0800

    ADD:tb_Aura.res Client

--- SWAdmin/Form1.cs
+++ SWAdmin/Form1.cs
@@ -245,6 +245,7 @@ namespace SWAdmin
             _supportedFiles.Add(ToLow("tb_Talk.res"), new tb_Talk_Client());
             _supportedFiles.Add(ToLow("tb_Talk_List.res"), new tb_Talk_List_Client());
             _supportedFiles.Add(ToLow("tb_Title_Info.res"), new tb_Title_Info_Client());
+            _supportedFiles.Add(ToLow("tb_Aura.res"), new tb_Aura_Client());
 
         }
         string ToLow(string input) {
---
+++ SWAdmin/TableStruct/Client/tb_Aura.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAdmin.TableStruct
+{
+    public class tb_Aura_Client : BaseStruct
+    {
+        public tb_Aura[] lsData;
+        public SWString md5_ = new SWString();
+
+        public override void beforeRead()
+        {
+        }
+
+        public override void beforeWrite()
+        {
+        }
+
+        public override void read(SWReader reader)
+        {
+        }
+
+        public override void write(SWWriter writer)
+        {
+        }
+
+        public class tb_Aura : BaseStruct
+        {
+            public UInt16 Unknown1;
+            public UInt16 Unknown2;
+            public UInt16 Unknown3;
+            public UInt16 Unknown4;
+            public UInt32 Unknown5;
+            public UInt16 Unknown6;
+            public UInt16 Unknown7;
+            public UInt16 Unknown8;
+            public UInt16 Unknown9;
+            public UInt16 Unknown10;
+
+            public override void beforeRead()
+            {
+            }
+
+            public override void beforeWrite()
+            {
+            }
+
+            public override void read(SWReader reader)
+            {
+            }
+
+            public override void write(SWWriter writer)
+            {
+            }
+        }
+    }
+}
