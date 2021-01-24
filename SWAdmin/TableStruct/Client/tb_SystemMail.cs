08069438f260cd15dd4d519dd4ee74ae8e235fd7
GreenDamTan <GreenDam@makedie.xyz>
2021-01-24 13:32:27 +0800

    ADD:tb_SystemMail.res Client

---
+++ SWAdmin/TableStruct/Client/tb_SystemMail.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAdmin.TableStruct
+{
+    public class tb_SystemMail_Client : BaseStruct
+    {
+        public tb_SystemMail[] lsData;
+        public SWString md5_ = new SWString();
+
+        public override void beforeRead()
+        {
+        }
+
+        public override void beforeWrite()
+        {
+        }
+
+        public override void read(SWReader reader)
+        {
+        }
+
+        public override void write(SWWriter writer)
+        {
+        }
+
+        public class tb_SystemMail : BaseStruct
+        {
+            public Byte Unknown1;
+            public UInt16 Unknown2;
+            public UInt16 Unknown3;
+            public String Unknown4 = "";
+            public String Unknown5 = "";
+            public String Unknown6 = "";
+            public String Unknown7 = "";
+
+            public override void beforeRead()
+            {
+            }
+
+            public override void beforeWrite()
+            {
+            }
+
+            public override void read(SWReader reader)
+            {
+            }
+
+            public override void write(SWWriter writer)
+            {
+            }
+        }
+    }
+}
