22f2bd3cc0a789d83511edf2663f307fdbe93668
GreenDamTan <GreenDam@makedie.xyz>
2021-01-24 22:20:25 +0800

    ADD:tb_Equalizer_Info.res Client

--- SWAdmin/Form1.cs
+++ SWAdmin/Form1.cs
@@ -256,6 +256,7 @@ namespace SWAdmin
             _supportedFiles.Add(ToLow("tb_Quest_Episode.res"), new tb_Quest_Episode_Client());
             _supportedFiles.Add(ToLow("tb_Character_Info.res"), new tb_Character_Info_Client());
             _supportedFiles.Add(ToLow("tb_change_tab.res"), new tb_change_tab_Client());
+            _supportedFiles.Add(ToLow("tb_Equalizer_Info.res"), new tb_Equalizer_Info_Client());
 
         }
         string ToLow(string input) {
