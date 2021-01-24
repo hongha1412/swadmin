5a53882fb408bd51c9bca5dcdbd85a9a529643a3
GreenDamTan <GreenDam@makedie.xyz>
2021-01-24 14:52:49 +0800

    ADD: tb_Class_form.res Client

--- SWAdmin/Form1.cs
+++ SWAdmin/Form1.cs
@@ -247,6 +247,7 @@ namespace SWAdmin
             _supportedFiles.Add(ToLow("tb_Title_Info.res"), new tb_Title_Info_Client());
             _supportedFiles.Add(ToLow("tb_Aura.res"), new tb_Aura_Client());
             _supportedFiles.Add(ToLow("tb_Cinema_String.res"), new tb_Cinema_String_Client());
+            _supportedFiles.Add(ToLow("tb_Class_form.res"), new tb_Class_form_Client());
 
         }
         string ToLow(string input) {
---
+++ SWAdmin/TableStruct/Client/tb_Class_form.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAdmin.TableStruct
+{
+    public class tb_Class_form_Client : BaseStruct
+    {
+        public tb_Class_form[] lsData;
+        public SWString md5_ = new SWString();
+
+        public override void beforeRead()
+        {
+        }
+
+        public override void beforeWrite()
+        {
+        }
+
+        public override void read(SWReader reader)
+        {
+        }
+
+        public override void write(SWWriter writer)
+        {
+        }
+
+        public class tb_Class_form : BaseStruct
+        {
+            public UInt32 Unknown1;
+            public String Unknown2 = "";
+            public override void beforeRead()
+            {
+            }
+
+            public override void beforeWrite()
+            {
+            }
+
+            public override void read(SWReader reader)
+            {
+            }
+
+            public override void write(SWWriter writer)
+            {
+            }
+        }
+    }
+}
