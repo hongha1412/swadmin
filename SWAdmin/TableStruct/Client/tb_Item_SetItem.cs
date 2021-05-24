8d4a7146a3fb173e9290f031a5d485c0e24f7641
GreenDamTan <GreenDam@makedie.xyz>
2021-05-24 22:29:39 +0800

    ADD:tb_Item_SetItem client

---
+++ SWAdmin/TableStruct/Client/tb_Item_SetItem.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAdmin.TableStruct
+{
+    public class tb_Item_SetItem_Client : BaseStruct
+    {
+        public tb_Item_SetItem[] lsData;
+        public SWString md5_ = new SWString();
+
+        public override void beforeRead()
+        {
+        }
+
+        public override void beforeWrite()
+        {
+        }
+
+        public override void read(SWReader reader)
+        {
+        }
+
+        public override void write(SWWriter writer)
+        {
+        }
+
+        public class tb_Item_SetItem : BaseStruct
+        {
+            public UInt32 Unkown1;
+            public UInt16 Unkown2;
+            public Byte Unkown3;
+            public UInt32 Unkown4;
+            public UInt32 Unkown5;
+            public UInt32 Unkown6;
+            public UInt32 Unkown7;
+            public UInt32 Unkown8;
+            public UInt32 Unkown9;
+            public Byte Unkown10;
+            public Byte Unkown11;
+            public Byte Unkown12;
+            public Byte Unkown13;
+            public Byte Unkown14;
+            public Byte Unkown15;
+            public Byte Unkown16;
+            public Byte Unkown17;
+            public Byte Unkown18;
+            public Byte Unkown19;
+            public UInt16 Unkown20;
+            public UInt16 Unkown21;
+            public UInt16 Unkown22;
+            public UInt16 Unkown23;
+            public UInt16 Unkown24;
+            public Single Unkown25;
+            public Single Unkown26;
+            public Single Unkown27;
+            public Single Unkown28;
+            public Single Unkown29;
+            public Byte Unkown30;
+            public Byte Unkown31;
+            public Byte Unkown32;
+            public Byte Unkown33;
+            public Byte Unkown34;
+            public UInt16 Unkown35;
+            public UInt16 Unkown36;
+            public UInt16 Unkown37;
+            public UInt16 Unkown38;
+            public UInt16 Unkown39;
+            public Single Unkown40;
+            public Single Unkown41;
+            public Single Unkown42;
+            public Single Unkown43;
+            public Single Unkown44;
+            public Byte Unkown45;
+            public Byte Unkown46;
+            public Byte Unkown47;
+            public Byte Unkown48;
+            public Byte Unkown49;
+            public UInt16 Unkown50;
+            public UInt16 Unkown51;
+            public UInt16 Unkown52;
+            public UInt16 Unkown53;
+            public UInt16 Unkown54;
+            public Single Unkown55;
+            public Single Unkown56;
+            public Single Unkown57;
+            public Single Unkown58;
+            public Single Unkown59;
+            public Byte Unkown60;
+            public Byte Unkown61;
+            public Byte Unkown62;
+            public Byte Unkown63;
+            public Byte Unkown64;
+            public UInt16 Unkown65;
+            public UInt16 Unkown66;
+            public UInt16 Unkown67;
+            public UInt16 Unkown68;
+            public UInt16 Unkown69;
+            public Single Unkown70;
+            public Single Unkown71;
+            public Single Unkown72;
+            public Single Unkown73;
+            public Single Unkown74;
+
+
+            public override void beforeRead()
+            {
+            }
+
+            public override void beforeWrite()
+            {
+            }
+
+            public override void read(SWReader reader)
+            {
+            }
+
+            public override void write(SWWriter writer)
+            {
+            }
+        }
+    }
+}
